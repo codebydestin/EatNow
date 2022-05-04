6ea7f00e529c41b901305e51e8464e56a8fcf7d4
codebydestin <destin.yanaa@gmail.com>
2022-05-04 03:14:49 +0100

    add readme file

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-import { AppState, Text, View } from "react-native";
-
 import Navigation from "./Navigation";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return AppState.currentState === "active" && <Navigation />;
+  return <Navigation />;
 }
